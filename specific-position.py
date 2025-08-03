e4d31121ab88923b01da8c2b7662ac1c3ea753b8
pavangorpena1404 <pavangorpena@gmail.com>
2025-08-03 23:51:15 +0530

    solved to arrange value specific posision in a list

---
+++ specific-position.py
@@ -0,0 +1,35 @@
+
+# Q) Write a Java program to insert an element (specific position) into an array.
+
+
+number= [12,3,49,5,8,94,66,20]
+number.insert(3,100)
+
+print(number)
+
+
+# Q) this is another way without using method
+
+
+number = [12, 3, 49, 5, 8, 94, 66, 20]
+posision=3
+element=100
+
+new_arr=[]
+for i in range(posision):
+    new_arr.append(number[i])
+    
+new_arr.append(element)
+print(new_arr)
+
+for i in range(posision,len(number)):
+    new_arr.append(number[i])
+    
+print(new_arr)   
+
+
+
+
+
+
+
